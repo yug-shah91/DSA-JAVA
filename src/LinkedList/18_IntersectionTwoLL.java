f66dcd3888b9a0601ddd9a07341c56eb6335e95d
Yug Shah <shahyug91@gmail.com>
2026-04-13 19:34:00 +0530

    Insertion Of Two LL

---
+++ src/LinkedList/18_IntersectionTwoLL.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+class IntersectionTwoLL {
+
+    public class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) {
+         val = x;
+         next = null;
+     }
+ }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+
+        while (p1 != p2) {
+            p1 = (p1 == null) ? headB : p1.next;
+            p2 = (p2 == null) ? headA : p2.next;
+        }
+
+        return p1;
+    }
+}
